47c68cb2cc3faa59d0477bf44909a8b004278006
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-06-16 09:23:09 +0500

    1-4 lessons done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js,jsx}"],
+  purge: [
+    // Add your HTML files
+    "./lesson4.html",
+    // Add your JavaScript files
+    "./src/**/*.js",
+    // ...
+  ],
   theme: {
     extend: {},
   },
